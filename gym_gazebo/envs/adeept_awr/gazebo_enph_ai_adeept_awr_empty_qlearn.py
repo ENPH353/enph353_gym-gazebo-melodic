7f353d346766d52bee0c3629c121fa4b11ee4bf1
Tyler Lum <tylergwlum@gmail.com>
2019-07-06 03:38:46 -0700

    Change reset_simulation to reset_world to fix bug, could also fix with rm updateRate

--- gym_gazebo/envs/adeept_awr/gazebo_enph_ai_adeept_awr_empty_qlearn.py
+++ gym_gazebo/envs/adeept_awr/gazebo_enph_ai_adeept_awr_empty_qlearn.py
@@ -21,7 +21,8 @@ class Gazebo_ENPH_Ai_Adeept_Awr_Empty_Env(gazebo_env.GazeboEnv):
         self.vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
         self.unpause = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
         self.pause = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
-        self.reset_proxy = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
+        # self.reset_proxy = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
+        self.reset_proxy = rospy.ServiceProxy('/gazebo/reset_world', Empty)
 
         self.action_space = spaces.Discrete(3) #F,L,R
         self.reward_range = (-np.inf, np.inf)
@@ -59,18 +60,18 @@ class Gazebo_ENPH_Ai_Adeept_Awr_Empty_Env(gazebo_env.GazeboEnv):
 
         if action == 0: #FORWARD
             vel_cmd = Twist()
-            vel_cmd.linear.x = 0.3
+            vel_cmd.linear.x = 0.8
             vel_cmd.angular.z = 0.0
             self.vel_pub.publish(vel_cmd)
         elif action == 1: #LEFT
             vel_cmd = Twist()
             vel_cmd.linear.x = 0
-            vel_cmd.angular.z = 0.3
+            vel_cmd.angular.z = 3
             self.vel_pub.publish(vel_cmd)
         elif action == 2: #RIGHT
             vel_cmd = Twist()
             vel_cmd.linear.x = 0
-            vel_cmd.angular.z = -0.3
+            vel_cmd.angular.z = -3
             self.vel_pub.publish(vel_cmd)
 
         data = None
@@ -102,12 +103,14 @@ class Gazebo_ENPH_Ai_Adeept_Awr_Empty_Env(gazebo_env.GazeboEnv):
     def reset(self):
 
         # Resets the state of the environment and returns an initial observation.
-        rospy.wait_for_service('/gazebo/reset_simulation')
+        # rospy.wait_for_service('/gazebo/reset_simulation')
+        rospy.wait_for_service('/gazebo/reset_world')
         try:
             #reset_proxy.call()
             self.reset_proxy()
         except (rospy.ServiceException) as e:
-            print ("/gazebo/reset_simulation service call failed")
+            # print ("/gazebo/reset_simulation service call failed")
+            print ("/gazebo/reset_world service call failed")
 
         # Unpause simulation to make observation
         rospy.wait_for_service('/gazebo/unpause_physics')
