7c4671d09df9af69c8aee0aae66d2231a2956c54
Tyler Lum <tylergwlum@gmail.com>
2019-08-29 10:07:10 -0700

    Use #gray px dir in front as reward and # white pixels in each histogram as state

--- gym_gazebo/envs/adeept_awr/gazebo_enph_ai_adeept_awr_empty_qlearn.py
+++ gym_gazebo/envs/adeept_awr/gazebo_enph_ai_adeept_awr_empty_qlearn.py
@@ -52,8 +52,46 @@ class Gazebo_ENPH_Ai_Adeept_Awr_Empty_Env(gazebo_env.GazeboEnv):
 
         self.prev_lines = None
 
+    def four_point_transform(self, image, rect):
+      # obtain a consistent order of the points and unpack them
+      # individually
+      (tl, tr, br, bl) = rect
+     
+      # compute the width of the new image, which will be the
+      # maximum distance between bottom-right and bottom-left
+      # x-coordiates or the top-right and top-left x-coordinates
+      widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+      widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+      maxWidth = max(int(widthA), int(widthB))
+     
+      # compute the height of the new image, which will be the
+      # maximum distance between the top-right and bottom-right
+      # y-coordinates or the top-left and bottom-left y-coordinates
+      heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+      heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+      maxHeight = max(int(heightA), int(heightB))
+     
+      # now that we have the dimensions of the new image, construct
+      # the set of destination points to obtain a "birds eye view",
+      # (i.e. top-down view) of the image, again specifying points
+      # in the top-left, top-right, bottom-right, and bottom-left
+      # order
+      dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype = "float32")
+     
+      # compute the perspective transform matrix and then apply it
+      M = cv2.getPerspectiveTransform(rect, dst)
+      warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+     
+      # return the warped image
+      return warped
+
     def process_image(self, image_data):
 
+        # Get image
         try:
           image = self.bridge.imgmsg_to_cv2(image_data, "bgr8")
         except CvBridgeError as e:
@@ -78,63 +116,44 @@ class Gazebo_ENPH_Ai_Adeept_Awr_Empty_Env(gazebo_env.GazeboEnv):
         rect[3][0] = 172
         rect[3][1] = 720
 
-        def four_point_transform(image, rect):
-          # obtain a consistent order of the points and unpack them
-          # individually
-          (tl, tr, br, bl) = rect
-         
-          # compute the width of the new image, which will be the
-          # maximum distance between bottom-right and bottom-left
-          # x-coordiates or the top-right and top-left x-coordinates
-          widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-          widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-          maxWidth = max(int(widthA), int(widthB))
-         
-          # compute the height of the new image, which will be the
-          # maximum distance between the top-right and bottom-right
-          # y-coordinates or the top-left and bottom-left y-coordinates
-          heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-          heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-          maxHeight = max(int(heightA), int(heightB))
-         
-          # now that we have the dimensions of the new image, construct
-          # the set of destination points to obtain a "birds eye view",
-          # (i.e. top-down view) of the image, again specifying points
-          # in the top-left, top-right, bottom-right, and bottom-left
-          # order
-          dst = np.array([
-            [0, 0],
-            [maxWidth - 1, 0],
-            [maxWidth - 1, maxHeight - 1],
-            [0, maxHeight - 1]], dtype = "float32")
-         
-          # compute the perspective transform matrix and then apply it
-          M = cv2.getPerspectiveTransform(rect, dst)
-          warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-         
-          # return the warped image
-          return warped
+        # Get ortho image
+        warped = self.four_point_transform(image, rect)
 
-        warped = four_point_transform(image, rect)
+        # Get reward based on number of gray pixels
+        region_of_interest = warped[warped.shape[0]*4//5:warped.shape[0],0:warped.shape[1]]
+        cv2.imshow("Image window", region_of_interest)
+        cv2.waitKey(3)
+        #l = np.sum(region_of_interest == 83)
+        #k = np.sum(region_of_interest == 84)
+        #j = np.sum(region_of_interest == 85)
+        #print(region_of_interest[region_of_interest.shape[0]//2,region_of_interest.shape[1]//2,0])
+        #print(region_of_interest[region_of_interest.shape[0]//2,region_of_interest.shape[1]//2,1])
+        #print(region_of_interest[region_of_interest.shape[0]//2,region_of_interest.shape[1]//2,2])
+        reward = np.sum((region_of_interest > 35) & (region_of_interest < 90)) / (255*255)
 
-        #cv2.imshow("Image window", warped)
-        #cv2.waitKey(3)
-        ### Get histogram
+        ### Get histogram for state
 
-        num_cols = 4
+        num_cols = 5
         w = warped.shape[1] / num_cols
         num_white = []
         for i in range(num_cols):
             cropped_img = warped[0:warped.shape[0], i*w:(i+1)*w]
             num_white.append(np.sum(cropped_img == 255))
 
-        print(num_white.index(max(num_white)))
+        import copy
+        copy_num_white = copy.copy(num_white)
+        copy_num_white.sort() 
+        for i, num in enumerate(copy_num_white):
+            index = num_white.index(num)
+            num_white[index] = i
 
-        state = []
+        state = num_white
         success = False
         fail = False
 
-        return state, success, fail
+        print("State: {}".format(state))
+        print("Reward: {}".format(reward))
+        return state, reward, success, fail
 
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -177,10 +196,9 @@ class Gazebo_ENPH_Ai_Adeept_Awr_Empty_Env(gazebo_env.GazeboEnv):
         except (rospy.ServiceException) as e:
             print ("/gazebo/pause_physics service call failed")
 
-        state, succeeded, failed = self.process_image(image_data)
+        state, reward, succeeded, failed = self.process_image(image_data)
 
         # Reward function
-        reward = 1
         return state, reward, (succeeded or failed), {}
 
     def reset(self):
@@ -211,6 +229,6 @@ class Gazebo_ENPH_Ai_Adeept_Awr_Empty_Env(gazebo_env.GazeboEnv):
         except (rospy.ServiceException) as e:
             print ("/gazebo/pause_physics service call failed")
 
-        state, succeeded, failed = self.process_image(image_data)
+        state, reward, succeeded, failed = self.process_image(image_data)
 
         return state
