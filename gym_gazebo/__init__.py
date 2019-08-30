e5cc203f7e93bb25fa2a63e388ebdb3af8a85299
Tyler Lum <tylergwlum@gmail.com>
2019-08-29 17:53:10 -0700

    Remove turtlebot and unittest from envs and logger and shorten __init__.py

--- gym_gazebo/__init__.py
+++ gym_gazebo/__init__.py
@@ -18,39 +18,6 @@ register(
     max_episode_steps=300,
 )
 
-# Turtlebot envs
-register(
-    id='GazeboMazeTurtlebotLidar-v0',
-    entry_point='gym_gazebo.envs.turtlebot:GazeboMazeTurtlebotLidarEnv',
-    # More arguments here
-)
-register(
-    id='GazeboCircuitTurtlebotLidar-v0',
-    entry_point='gym_gazebo.envs.turtlebot:GazeboCircuitTurtlebotLidarEnv',
-    # More arguments here
-)
-register(
-    id='GazeboCircuit2TurtlebotLidar-v0',
-    entry_point='gym_gazebo.envs.turtlebot:GazeboCircuit2TurtlebotLidarEnv',
-    # More arguments here
-)
-register(
-    id='GazeboCircuit2TurtlebotLidarNn-v0',
-    entry_point='gym_gazebo.envs.turtlebot:GazeboCircuit2TurtlebotLidarNnEnv',
-    max_episode_steps=1000,
-    # More arguments here
-)
-register(
-    id='GazeboCircuit2cTurtlebotCameraNnEnv-v0',
-    entry_point='gym_gazebo.envs.turtlebot:GazeboCircuit2cTurtlebotCameraNnEnv',
-    # More arguments here
-)
-register(
-    id='GazeboRoundTurtlebotLidar-v0',
-    entry_point='gym_gazebo.envs.turtlebot:GazeboRoundTurtlebotLidarEnv',
-    # More arguments here
-)
-
 # cart pole
 register(
     id='GazeboCartPole-v0',
